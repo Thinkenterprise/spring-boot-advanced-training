20f3fc7d16002988f7669c3fe5b87fefa7d90e52
thinkenterprise <michael.schaefer@msg-systems.com>
2020-04-12 13:54:46 +0200

    Change in Prometheus Script

--- s6-spring-boot-advanced-actuator/spring-boot-advanced-actuator-final/start-docker-with-prometheus.sh
+++ s6-spring-boot-advanced-actuator/spring-boot-advanced-actuator-final/start-docker-with-prometheus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d -p 9090:9090 -p 8080:8080 -v ~/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run -d -p 9090:9090 -v ~/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
