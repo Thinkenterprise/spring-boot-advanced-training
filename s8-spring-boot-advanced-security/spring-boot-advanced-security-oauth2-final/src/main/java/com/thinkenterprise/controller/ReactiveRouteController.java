9288f464a9046ce7a4a640fb45808a06b5475a09
Michael Schaefer <Michael.Schaefer@msg.group>
2020-10-03 22:46:48 +0200

    reactive security fix

---
+++ s8-spring-boot-advanced-security/spring-boot-advanced-security-oauth2-final/src/main/java/com/thinkenterprise/controller/ReactiveRouteController.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2020 Thinkenterprise
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author Michael Schaefer
+ */
+
+package com.thinkenterprise.controller;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thinkenterprise.domain.route.Route;
+
+import reactor.core.publisher.Flux;
+
+
+@RestController
+@RequestMapping("routes")
+@Profile("reactive")
+public class ReactiveRouteController {
+
+		
+	@RequestMapping
+	@PreAuthorize("hasAuthority('SCOPE_read')")
+	public Flux<Route> routes() {
+		
+		return Flux.just(new Route("LH7902","MUC","IAH"), 
+					new Route("LH1602","MUC","IBZ"), 
+					new Route("LH401","FRA","NYC"));
+	
+	}
+	 
+}
