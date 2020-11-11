5d11246521bf08e31bad58d9fe78c3e95e3891fc
Thinkenterprise <michael.schaefer@msg-systems.com>
2020-11-11 13:28:59 +0100

    Bug Fix in Kotlin Final Test

--- s9-spring-boot-advanced-kotlin/spring-boot-advanced-kotlin-final/src/main/kotlin/org/thinkenterprise/HelloWorldApplication.kt
+++
@@ -1,7 +0,0 @@
-package org.thinkenterprise
-
-	
-fun main(args: Array<String>) {
-	 System.out.print("Hello World");
-}
-	 
--- s9-spring-boot-advanced-kotlin/spring-boot-advanced-kotlin-final/src/main/kotlin/org/thinkenterprise/KotlinApplication.kt
+++ s9-spring-boot-advanced-kotlin/spring-boot-advanced-kotlin-final/src/main/kotlin/org/thinkenterprise/KotlinApplication.kt
@@ -5,9 +5,11 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.thinkenterprise.repository.RouteRepository
+import org.springframework.boot.SpringBootConfiguration
 
 @EnableAutoConfiguration
 @ComponentScan
+@SpringBootConfiguration
 class KotlinApplication 
 		
 	fun main(args: Array<String>) {
