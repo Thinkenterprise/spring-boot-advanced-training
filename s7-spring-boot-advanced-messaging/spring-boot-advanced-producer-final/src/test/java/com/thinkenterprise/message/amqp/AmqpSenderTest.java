09f9a30eec1a63c8f79772060e15d47c2af4a16c
Michael Kagel <michael.kagel@msg.group>
2024-04-21 13:53:38 +0200

    Add AmqpSender test with testcontainers

---
+++ s7-spring-boot-advanced-messaging/spring-boot-advanced-producer-final/src/test/java/com/thinkenterprise/message/amqp/AmqpSenderTest.java
@@ -0,0 +1,44 @@
+package com.thinkenterprise.message.amqp;
+
+
+import com.thinkenterprise.domain.tracking.FlightStatus;
+import com.thinkenterprise.domain.tracking.Tracking;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+@Testcontainers
+@SpringBootTest
+class AmqpSenderTest {
+
+    @Container
+    @ServiceConnection
+    static RabbitMQContainer rabbitMQContainer = new RabbitMQContainer("rabbitmq:3-management");
+
+    @Autowired
+    AmqpSender sender;
+
+    @Test
+    void containerStartAndRunning() {
+        assertThat(rabbitMQContainer.isCreated()).isTrue();
+        assertThat(rabbitMQContainer.isRunning()).isTrue();
+    }
+
+    @Test
+    void sendThrowsNoException() {
+        var tracking = new Tracking();
+        tracking.setId(1L);
+        tracking.setStatus(FlightStatus.BOARDING);
+        tracking.setFlightNumber("LH123");
+        tracking.setRouteId(4L);
+
+        assertThatNoException().isThrownBy(()->sender.sendMessage(tracking));
+    }
+}
