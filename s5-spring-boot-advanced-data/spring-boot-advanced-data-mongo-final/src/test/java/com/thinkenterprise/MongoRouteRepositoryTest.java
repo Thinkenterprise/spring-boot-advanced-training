12e8c276944669022f189ad63b24dd1a0d9157d1
Thinkenterprise <michael.schaefer@msg-systems.com>
2025-06-27 12:37:16 +0200

    Testcontainer in Mongo DB

--- s5-spring-boot-advanced-data/spring-boot-advanced-data-mongo-final/src/test/java/com/thinkenterprise/MongoRouteRepositoryTest.java
+++ s5-spring-boot-advanced-data/spring-boot-advanced-data-mongo-final/src/test/java/com/thinkenterprise/MongoRouteRepositoryTest.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.ComponentScan;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -17,26 +14,23 @@ import org.testcontainers.utility.DockerImageName;
 import com.thinkenterprise.repository.mongodb.MongoRouteRepository;
 
 @Testcontainers
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@DataMongoTest
-@ComponentScan(basePackageClasses = MongoRouteRepository.class)
+@SpringBootTest
 public class MongoRouteRepositoryTest {
 
 	
 	@Container
-    //@ServiceConnection
-    MongoDBContainer mongoDB = new MongoDBContainer(DockerImageName.parse("mongo:5.0"));
+    @ServiceConnection
+    public static MongoDBContainer  mongoDB = new MongoDBContainer(DockerImageName.parse("mongo:5.0"));
 
+	@Autowired
+	MongoRouteRepository mongoRouteRepository;
+		
 	@BeforeEach
     void connectionEstablished() {
 		Assertions.assertTrue(mongoDB.isCreated());
 		Assertions.assertTrue(mongoDB.isRunning());
     }
-	
-	
-	@Autowired
-	MongoRouteRepository mongoRouteRepository;
-	
+		
 	@Test
 	public void testGetAll() {
 		Assertions.assertTrue(mongoRouteRepository.findAll().size()==3);
